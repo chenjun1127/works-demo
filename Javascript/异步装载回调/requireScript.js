262a487d930d3c3138bd161fb04bfc14ac71df07
chenjun <chenjun@oeasy.com>
2017-09-06 09:42:27 +0800

    update

--- Javascript/异步装载回调/requireScript.js
+++ Javascript/异步装载回调/requireScript.js
@@ -1,34 +1,34 @@
-var Loader  = function () {};
+var Loader = function() {};
 Loader.prototype = {
-	require : function (scripts, callback) {
-		this.loadCount = 0;
-		this.totalRequired = scripts.length;
-		this.callback = callback;
-		for (var i = 0; i < scripts.length; i++) {
-			this.writeScript(scripts[i]);
-		}
-	},
-	loaded : function (evt) {
-		this.loadCount++;
-		if (this.loadCount == this.totalRequired && typeof this.callback == 'function')
-			this.callback.call();
-	},
-	writeScript : function (src) {
-		var self = this;
-		var s = document.createElement("script");
-		s.type = "text/javascript";
-		s.async = true;
-		s.src = src;
-		if (s.addEventListener) {
-			s.addEventListener("load", function (e) {
-				self.loaded(e);
-			}, false);
-		} else if (s.attachEvent) {
-			s.attachEvent("onload", function (e) {
-				self.loaded(e);
-			})
-		}
-		var head = document.getElementsByTagName('head')[0];
-		head.appendChild(s);
-	}
+    require: function(scripts, callback) {
+        this.loadCount = 0;
+        this.totalRequired = scripts.length;
+        this.callback = callback;
+        for (var i = 0; i < scripts.length; i++) {
+            this.writeScript(scripts[i]);
+        }
+    },
+    loaded: function(evt) {
+        this.loadCount++;
+        if (this.loadCount == this.totalRequired && typeof this.callback == 'function')
+            this.callback.call();
+    },
+    writeScript: function(src) {
+        var self = this;
+        var s = document.createElement("script");
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = src;
+        if (s.addEventListener) {
+            s.addEventListener("load", function(e) {
+                self.loaded(e);
+            }, false);
+        } else if (s.attachEvent) {
+            s.attachEvent("onload", function(e) {
+                self.loaded(e);
+            })
+        }
+        var head = document.getElementsByTagName('head')[0];
+        head.appendChild(s);
+    }
 }
